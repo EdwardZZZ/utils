8a13e2a1257a705fa44a0ccf0d43b40554c58cf4
大华 <zzh627@126.com>
2017-05-12 11:39:35 +0800

    Create z.js

---
+++ z.js
@@ -0,0 +1,15 @@
+                menus.forEach(function foreach(...props) {
+                    const prefix = typeof props[0] === 'string' ? props[0] : '',
+                        menu = prefix !== '' ? props[1] : props[0];
+
+                    const {id, name} = menu;
+                    _parentMenuList.push({id, name: prefix + name});
+
+                    if(id == self.menu.parent_id){
+                        self.parentMenuName = name;
+                    }
+
+                    if(menu.children && menu.children.length > 0){
+                        menu.children.forEach(foreach.bind(this, prefix + '----'));
+                    }
+                });

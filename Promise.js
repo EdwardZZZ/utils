9efbcb9e023d166438170fa5c024174f98c13eb4
大华 <zzh627@126.com>
2018-03-09 10:58:25 +0800

    Update Promise.js

--- Promise.js
+++ Promise.js
@@ -1,97 +1,101 @@
+const PENDDING = 'pending';
+const FULFILLED = 'fulfilled';
+const REJECTED = 'rejected';
+
+const _handle = Symbol('handle');
+const _deferred = Symbol('deferred');
+
 export default class Promise {
     constructor(fn) {
-        if (!fn) throw new TypeError('Promise resolver undefined is not a function');
+        if (!fn) {
+            throw new TypeError("Promise resolver undefined is not a function");
+        }
 
-        const resolve = (newValue) => {
+        const resolve = newValue => {
             if (newValue instanceof Promise) {
-                newValue.then(resolve);
+                if (newValue.status === REJECTED) {
+                    newValue.catch(reject);
+                } else {
+                    newValue.then(resolve);
+                }
                 return;
             }
-            this.status = 'fulfilled';
+            this.status = FULFILLED;
             this.value = newValue;
             finale();
-        }
+        };
 
-        const reject = (reason) => {
-            this.status = 'rejected';
+        const reject = reason => {
+            this.status = REJECTED;
             this.value = reason;
             finale();
-        }
+        };
 
         const finale = () => {
-            setTimeout(t => this.handle(this.deferred), 0);
-        }
+            // setImmediate 为macroTask，此处应该用microTask实现
+            // web用MutationObserver，node端用process.nextTick
+            setImmediate(t => this[_handle](this[_deferred]));
+        };
 
         fn(resolve, reject);
     }
 
-    status = 'pending';
+    status = PENDDING;
     value = void 0;
-    deferred = null;
 
-    static resolve = (d) => new Promise(resolve => resolve(d));
-    static reject = (d) => new Promise((resolve, reject) => reject(d));
-    static all = (promises) => new Promise((resolve, reject) => {
-        let results = [],
-            n = 0;
+    [_deferred] = null;
+    [_handle] = deferred => {
+        let { status, value } = this;
+
+        if (status === PENDDING) {
+            this[_deferred] = deferred;
+            return;
+        }
+
+        if (!deferred) return;
+
+        const { didFulfill, didReject, resolve, reject } = deferred;
+        let callback = status === FULFILLED ? didFulfill : didReject;
+        if (callback === null) {
+            callback = status === FULFILLED ? resolve : reject;
+            callback && callback(value);
+            return;
+        }
+
+        try {
+            const ret = callback(value);
+            resolve && resolve(ret);
+        } catch (e) {
+            reject && reject(e);
+        }
+    };
+
+    then(didFulfill = null, didReject = null) {
+        return new Promise((resolve, reject) => {
+            this[_handle]({ didFulfill, didReject, resolve, reject });
+        });
+    }
+
+    catch(didReject = null) {
+        return new Promise((resolve, reject) => {
+            this[_handle]({ didFulfill: null, didReject, resolve, reject });
+        });
+    }
+
+    static resolve = d => new Promise(resolve => resolve(d));
+    static reject = d => new Promise((resolve, reject) => reject(d));
+    static all = promises => new Promise((resolve, reject) => {
+        const results = [];
+        let n = 0;
         promises.forEach((promise, i) => {
             promise.then(data => {
                 results[i] = data;
                 if (++n === promises.length) {
                     resolve(results);
                 }
+            }, e => {
+                reject(e);
             });
         });
     });
-
-    handle = (deferred) => {
-        let { status, value } = this;
-
-        if (status === 'pending') {
-            this.deferred = deferred;
-            return;
-        }
-
-        if (!deferred) {
-            if (status === 'rejected') {
-                console.error(value);
-            }
-            return;
-        }
-
-        let cb = status === 'fulfilled' ? deferred.done : deferred.fail;
-        if (cb === null) {
-            cb = status === 'fulfilled' ? deferred.resolve : deferred.reject;
-            cb && cb(value);
-            return;
-        }
-
-        try {
-            const ret = cb(value);
-            deferred.resolve && deferred.resolve(ret);
-        } catch (e) {
-            deferred.reject && deferred.reject(e);
-        }
-    }
-
-    then(done, fail) {
-        return new Promise((resolve, reject) => {
-            this.handle({
-                done: done || null,
-                fail: fail || null,
-                resolve: resolve,
-                reject: reject
-            });
-        });
-    }
-
-    catch (fail) {
-        return new Promise((resolve, reject) => {
-            this.handle({
-                done: null,
-                fail: fail || null,
-                reject: reject
-            });
-        });
-    }
 }
